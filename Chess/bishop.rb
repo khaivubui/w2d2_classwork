95ec5043726cb067739ddff1a05c8c900d96297a
khaivubui <khaivubui@gmail.com>
2017-08-07 18:11:33 -0700

    skeleton with symbol

---
+++ Chess/bishop.rb
@@ -0,0 +1,16 @@
+require_relative 'moving_modules'
+require 'colorize'
+
+class Bishop < Piece
+  def initialize(starting_pos, board, color)
+    super
+  end
+
+  def symbol
+    if @color == :white
+      "\u{265D} ".light_white #white rook
+    else
+      "\u{265D} " #white rook
+    end
+  end
+end
