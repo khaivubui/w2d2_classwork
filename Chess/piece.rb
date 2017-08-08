53a12a94c71ec894f18ba1b4e75e0df39b6871c0
khaivubui <khaivubui@gmail.com>
2017-08-07 18:22:05 -0700

    step 4 complete

--- Chess/piece.rb
+++ Chess/piece.rb
@@ -1,5 +1,15 @@
 class Piece
-  def initialize
+  def initialize(starting_pos, board, color)
+    @pos = starting_pos # [row, col]
+    @board = board # board obj
+    @color = color
+  end
 
+  def moves
+    # return array of pos
+  end
+
+  def update_pos(new_pos)
+    @pos = new_pos
   end
 end
